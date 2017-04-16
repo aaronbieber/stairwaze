549f9a3f0ea9e7748a6e4d44328917ea23bedef1
Aaron Bieber <aaron@aaronbieber.com>
2017-04-16 12:27:29 -0400

    Do real network fetches for escalator and history data

--- actions/index.js
+++ actions/index.js
@@ -42,34 +42,13 @@ export const fetchEscalators = () => {
     dispatch(fetchingEscalators());
     console.log('fetching escalators');
 
-    const fakeItems = [
-      { id: 1, top: 'Currency Exchange', bottom: 'Sur La Table', up: true, down: false,
-        history: { up: [], down: [] }
-      },
-      { id: 2, top: 'Louis Vuitton', bottom: 'Au Bon Pain', up: true, down: true,
-        history: { up: [], down: [] }
-      },
-      { id: 3, top: 'Tiffany\'s', bottom: 'Marriott', up: true, down: true,
-        history: { up: [], down: [] }
-      },
-      { id: 4, top: 'Skylobby', bottom: 'Legal Sea Foods', up: true, down: true,
-        history: { up: [], down: [] }
-      },
-      { id: 5, top: 'Marriott', bottom: 'Star Market', up: true, down: true,
-        history: { up: [], down: [] }
-      },
-      { id: 6, top: 'Westin', bottom: 'Fogo De Chao', up: true, down: true,
-        history: { up: [], down: [] }
-      },
-      { id: 7, top: 'Gap', bottom: 'Tiffany\'s', up: true, down: true,
-        history: { up: [], down: [] }
-      }
-    ];
-
-    setTimeout(function() {
-      console.log('escalators fetched');
-      dispatch(setEscalators(fakeItems));
-    }, 1000);
+    // @todo Put a real domain in here, or configuration of some kind
+    fetch('http://192.168.10.81:5000/escalators')
+      .then((response) => response.json())
+      .then((responseJson) => dispatch(setEscalators(responseJson)))
+      .catch((error) => {
+        console.error(error);
+      });
   };
 };
 
@@ -92,37 +71,12 @@ export const fetchEscalatorHistory = (id, direction) => {
   return (dispatch) => {
     dispatch(fetchingEscalatorHistory());
 
-    setTimeout(function() {
-      console.log('history fetched');
-
-      var fakeHistory = {
-        1: {
-          up: [
-            { id: 1,
-              timestamp: '2017-04-07 18:00:00 EST',
-              event: 'broken' },
-            { id: 2,
-              timestamp: '2017-04-10 18:00:00 EST',
-              event: 'fixed' }
-          ],
-          down: [
-            { id: 1,
-              timestamp: '2017-04-07 18:00:00 EST',
-              event: 'broken' },
-            { id: 2,
-              timestamp: '2017-04-07 19:00:00 EST',
-              event: 'broken' },
-            { id: 3,
-              timestamp: '2017-04-10 09:00:00 EST',
-              event: 'fixed' },
-            { id: 4,
-              timestamp: '2017-04-10 10:00:00 EST',
-              event: 'fixed' }
-          ]
-        }
-      };
-
-      dispatch(setEscalatorHistory(id, direction, fakeHistory[id][direction]));
-    }, 1000);
+    // @todo Put a real domain in here, or configuration of some kind
+    fetch('http://192.168.10.81:5000/escalators/' + id + '/history/' + direction)
+      .then((response) => response.json())
+      .then((responseJson) => dispatch(setEscalatorHistory(id, direction, responseJson)))
+      .catch((error) => {
+        console.error(error);
+      });
   };
 };
